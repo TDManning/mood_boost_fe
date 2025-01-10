be1e85e2f3a71ecdd62f2067dbc61e90869a8113
Michael OBrien <obrien.michael.t@gmail.com>
2025-01-09 19:18:03 -0500

    Creates the navbar layout

--- mood_boost_fe/src/NavBar/NavBar.jsx
+++ mood_boost_fe/src/NavBar/NavBar.jsx
@@ -1,22 +1,29 @@
-import homebtn from "../icons/homebtn"
-import "./NavBar.css";
 import { NavLink } from 'react-router-dom';
+import "./NavBar.css";
+import homebtn from '../icons/home.png';
+import HomePage from "../HomePage/HomePage";
+import BreathingPage from '../BreathingPage/BreathingPage';
+import QuotePage from '../QuotePage/QuotePage';
+import JokePage from '../JokePage/JokePage';
 
 
 function NavBar () {
     return (
-        <nav>
-            <NavLink to="/" className="home">
+        <nav className="navbar">
+            <NavLink to="/" element={ HomePage } className="home">
                 <img className="homebtn" src={ homebtn } alt="Back to home page" />
             </NavLink>
             <ul>
-                <li><NavLink ></NavLink></li>
-                <li><NavLink ></NavLink></li>
-                <li><NavLink ></NavLink></li>
+                <li>Mood Boosts
+                    <ul className="dropdown">
+                        <li><NavLink to="/breathing" element={ BreathingPage }>Breathing Exercies</NavLink></li>
+                        <li><NavLink to="/quote" element={ QuotePage }>Inspiration Quotes</NavLink></li>
+                        <li><NavLink to="/joke" element={ JokePage }>Joke Generator</NavLink></li>
+                    </ul>
+                </li>    
             </ul>
+            <button className="login">Login/Register</button> 
         </nav>
-        
-
     )
 }
 
