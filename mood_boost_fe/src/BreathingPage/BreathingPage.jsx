b2338940634606b7cdd7a65dbcf7394f892db0ee
Paul Knapp <ppankluap@gmail.com>
2025-01-10 20:46:41 -0600

    Feat: Breathing page css.

--- mood_boost_fe/src/BreathingPage/BreathingPage.jsx
+++ mood_boost_fe/src/BreathingPage/BreathingPage.jsx
@@ -4,7 +4,7 @@ import "./BreathingPage.css";
 function BreathingPage() {
   return (
     <div className="breathing-page">
-      <h1>Breathing Page</h1>
+      {/* <h1>Breathing Page</h1> */}
       <img 
         src="https://opryshok.com/wp-content/uploads/2023/10/Box-Breathing-GIF.gif"
         alt="Breathing Exercise"/>
