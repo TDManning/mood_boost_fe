8aeb04945f359c0c39d10ecbc1a94e9b22eb0005
Terra Manning <terradmanning@gmail.com>
2025-01-12 13:59:22 -0700

    feat: include an animation component and color changes to some elements

--- mood_boost_fe/src/HomePage/HomePage.jsx
+++ mood_boost_fe/src/HomePage/HomePage.jsx
@@ -1,9 +1,8 @@
 import "./HomePage.css";
-import { NavLink } from 'react-router-dom';
+import "./FloatingCircles.css";
+import FloatingCircles from "./FloatingCircles";
 import { useNavigate } from "react-router-dom";
 import React, { useState } from "react";
-import "react-spinning-wheel/dist/style.css";
-
 
 function HomePage() {
   const navigate = useNavigate(); 
@@ -16,26 +15,34 @@ function HomePage() {
   ];
 
   const spinWheel = () => {
+
+  const spinner = document.querySelector(".basic-spinner");
+  spinner.classList.add("active"); 
+
+  setTimeout(() => {
     const randomIndex = Math.floor(Math.random() * options.length); 
     const selectedOption = options[randomIndex]; 
     setResult(selectedOption.label);
     navigate(selectedOption.route);
-  };
+  
+    spinner.classList.remove("active");
+  }, 500); 
+};
 
   return (
     <div className="home-page-wrapper">
       <div className="home-page">
-        <h1>Welcome to Mood Boost</h1>
+      <FloatingCircles />
+        <h1 class="animated-title">Welcome to Mood Boost</h1>
         <h2>
           Mood Boost offers simple breathing exercises, inspiring quotes, and a
           touch of humor to brighten your day and put a smile on your face.
         </h2>
-        <p>Spin the wheel to see a random page or use the menu to navigate to different pages</p>
+        <p>Click below to see a random page or use the menu to navigate to different pages</p>
         <div className="basic-spinner" onClick={spinWheel}>
           <div className="spinner-outer"></div>
-          <p className="spinner-text">Spin Me!</p>
+          <p className="spinner-text"></p>
         </div>
-        {result && <p className="result">You got: {result}</p>}
       </div>
     </div>
   );
