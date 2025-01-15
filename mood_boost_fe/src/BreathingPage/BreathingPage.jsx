5061f2f1d8d825b5951267b3a62a35727495d1c7
Paul Knapp <ppankluap@gmail.com>
2025-01-15 12:07:48 -0600

    Feat: added button to start breathing (breathing not allowed before)

--- mood_boost_fe/src/BreathingPage/BreathingPage.jsx
+++ mood_boost_fe/src/BreathingPage/BreathingPage.jsx
@@ -1,13 +1,28 @@
 import FloatingCircles from "../HomePage/FloatingCircles";
 import "./BreathingPage.css";
+import { useState } from "react";
 
 
 function BreathingPage() {
+  const [isBreathing, setIsBreathing] = useState(false);
+
+  const handleStartBreathing = () => {
+    setIsBreathing(true);
+  }
+
   return (
     <div>
         <section class="breathing-page">
             <FloatingCircles class="background"/>
-            <div className="breathing-circle"></div>
+            {!isBreathing && (
+                <button classname="start-button"
+               onClick={handleStartBreathing}
+               >Start Breathing Exercise</button>
+            )}
+              {isBreathing && (
+                  <div className="breathing-circle"></div>
+              )}
+          
       </section>
     </div>
   );
